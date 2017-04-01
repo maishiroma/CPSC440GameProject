77fafe0fba2969d403a06858d65a4915786c0fda
shiro105 <shiro105@mail.chapman.edu>
2017-04-01 14:47:08 -0700

    Readded in autosaving and implementation of spatial audio

--- Assets/Scripts/SaveAndLoading/MenuControl.cs
+++ Assets/Scripts/SaveAndLoading/MenuControl.cs
@@ -37,7 +37,7 @@ public class MenuControl : MonoBehaviour {
 		}
 	}
 
-	// This is here for debugging purposes. In the actual game, these functions are tied to button clicks.
+	// This is here for debugging purposes.
 	void Update () {
 		if(Input.GetKey(KeyCode.I))
 			SaveGame();
