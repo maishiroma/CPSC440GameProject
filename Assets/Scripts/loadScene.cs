9e0542c027bd144b6473002e11c0c233d8b0222e
shiro105 <shiro105@mail.chapman.edu>
2017-04-01 13:44:33 -0700

    Added autosave/load and started spatial audio

--- Assets/Scripts/loadScene.cs
+++ Assets/Scripts/loadScene.cs
@@ -7,15 +7,27 @@ public class loadScene : MonoBehaviour {
     public string sceneName;
     public ScreenFade screenFade;
     public bool fadeIn = true;
+
+	// For GoogleVR Spatial Audio
+	public AudioClip levelSelectSound;
+	public GvrAudioSource levelSelectSource;
+
+
     void Start()
     {
         screenFade = GameObject.Find("ScreenFade").GetComponent<ScreenFade>();
+		levelSelectSource = GetComponent<GvrAudioSource>();
     }
 
     // Use this for initialization
     public void LoadScene ()
     {
-        screenFade.gameObject.SetActive(true);
+		// If the current scene is the Main Menu, the game autosaves.
+		if(SceneManager.GetActiveScene().name == "Main Menu")
+			MenuControl.Instance.SaveGame();
+
+
+		screenFade.gameObject.SetActive(true);
         screenFade.Fade(fadeIn);
         Invoke("Load", screenFade.fadeTime);
 	}
@@ -24,5 +36,11 @@ public class loadScene : MonoBehaviour {
     {
         SceneManager.LoadScene(sceneName);
     }
+
+	// Plays the sound for selecting a sound
+	public void PlaySelectSound()
+	{
+		levelSelectSource.PlayOneShot(levelSelectSound, 0.5f);
+	}
 
 }
