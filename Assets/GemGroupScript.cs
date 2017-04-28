e99e1fbef860c2313fa8909e7c90387cdd610d78
Cameron <horst106@mail.chapman.edu>
2017-04-28 11:28:53 -0700

    Gems

---
+++ Assets/GemGroupScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemGroupScript : MonoBehaviour {
+
+    public GameObject[] gems;
+    public float gemSpawnRadius;
+    public bool highlightingGems;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //get reference to parent (breakableRockScript)
+        //get local variables of breakable rock's min and max gems
+        //call CreateGems(min, max)
+	}
+
+
+    void CreateGems(int minGems, int maxGems)
+    {
+        //create random number of gems from min and max
+        //spawn gems in random point in unit sphere offset from this.transform
+    }
+
+    public void WakeGems()
+    {
+        //call from breakable rock script when rock breaks
+        //foreach of the gems, set them to active
+    }
+
+    public void collectGems()
+    {
+        //Call fred - Fred.CollectGems(Vector 3 position, WaypointScript waypoint)
+        //Disable Sphere Collider on Gems
+        //Add game sum mass to current materials in player manager
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GemScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour {
+
+    public int minDropAmt = 30;
+    public int maxDropAmt = 78;
+    public int DropAmt;
+
+    GemGroupScript GemGroup;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DropAmt = Random.Range(minDropAmt, maxDropAmt);
+	}
+	
+    public void ScanGem()
+    {
+        //GemGroupScript > CollectGems()
+    }
+
+    public void Highlight()
+    {
+        //if GemGroupScript > not highlighted ...  highlight()
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
