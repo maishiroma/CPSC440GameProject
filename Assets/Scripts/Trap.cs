72c35e9e7fa87271d1f88875b440ee8bdd9ec2f0
Matthew Shiroma <shiro105@mail.chapman.edu>
2017-04-30 19:51:13 -0700

    Revert "Sounds and trap"

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -8,7 +8,7 @@ public class Trap : MonoBehaviour {
     public float cost;
     public bool purchased;
     public GameObject icon;
-	public GameObject trapPrefab;		// Holds a prefab of the Trap to be equipped by the player.
+
 
     // -1 = not equipped 
     // 1 - left
@@ -16,13 +16,7 @@ public class Trap : MonoBehaviour {
     // 3 - right
     public int slot;
 
-	// Plays the trap placement animation
-	public void PlaceTrap()
-	{
-		//After releasing the button
-		if(gameObject.GetComponent<Animator>() != null)
-			gameObject.GetComponent<Animator>().SetTrigger("PlaceTrap");
-	}    
+            
 
 	
 }
